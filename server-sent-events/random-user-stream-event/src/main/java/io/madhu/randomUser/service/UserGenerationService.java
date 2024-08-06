adc4eb0ac1ce1eb905fc8ebd38274b66321f2db9
Gunda Madhukar <madhukar.b4u@gmail.com>
2024-08-07 00:34:24 +0800

    Removed Serversent events usecase from main branch

--- server-sent-events/random-user-stream-event/src/main/java/io/madhu/randomUser/service/UserGenerationService.java
+++
@@ -1,5 +0,0 @@
-package io.madhu.randomUser.service;
-
-public interface UserGenerationService<T> {
-    public T generateUser();
-}
--- server-sent-events/sse-sending-file-content/src/main/java/io/madhu/sendingFileContent/listener/FileListener.java
+++
@@ -1,5 +0,0 @@
-package io.madhu.sendingFileContent.listener;
-
-public interface FileListener {
-    void onFileDetected(String content);
-}
